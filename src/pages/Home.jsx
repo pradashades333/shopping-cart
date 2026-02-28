cba5e13ede5d56483fa5d210565778e8a5bd382d
pradashades333 <alexdreve03@gmail.com>
2026-02-28 22:44:19 +0100

    added home code and fixed issues

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return(
+        <div>
+            <h1>welcome to the store</h1>
+            <p>Selling coolest diddenbludden from Ohio rizzler</p>
+        </div>
+        
+    )
+}
+
+export default Home;
