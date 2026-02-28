a2616d50dc0aab1ce034ae5e14753088393067a4
pradashades333 <alexdreve03@gmail.com>
2026-02-28 22:22:17 +0100

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState([])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar cartCount = {cart.length} />
+      <Routes>
+        <Route path = "/" element = {<Home/>} />
+        <Route path = "/shop" element = {<Shop cart = {cart} setCart = {setCart}/>} />
+        <Route path = "/cart" element = {<Cart cart = {cart} setCart = {setCart}/>} />
+      </Routes> 
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+const Navbar = ({cartCount}) => {
+    return (
+        <nav>
+            <h1>Shopping store</h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/shop">Shop</Link>
+                </li>
+                <li>
+                    <Link to="/cart">
+                        Cart ({cartCount})
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
